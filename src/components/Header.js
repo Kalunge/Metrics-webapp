d8194d3a3bbfff60742354ec42b961ce8e0e4ef8
kalunge <muthomititus+github@gmail.com>
2021-10-14 09:00:16 +0100

    Fix linter errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-/* eslint-disable */
 import React from 'react';
 import { MdKeyboardVoice, MdSettings } from 'react-icons/md';
 import { IoIosArrowBack } from 'react-icons/io';
 import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const Header = ({ metric }) => {
   const history = useHistory();
@@ -12,13 +12,19 @@ const Header = ({ metric }) => {
   return (
     <div className="navBar mt-3 mb-3">
       <button type="button" onClick={handleClick}>
-        <IoIosArrowBack size={42} color={'pink'} />
+        <IoIosArrowBack size={42} color="pink" />
       </button>
-      <h2>{metric} </h2>
-      <MdKeyboardVoice size={42} color={'pink'} />
-      <MdSettings size={42} color={'pink'} />
+      <h2>
+        {metric}
+      </h2>
+      <MdKeyboardVoice size={42} color="pink" />
+      <MdSettings size={42} color="pink" />
     </div>
   );
+};
+
+Header.propTypes = {
+  metric: PropTypes.string.isRequired,
 };
 
 export default Header;
