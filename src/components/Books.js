e819bec7a7b8b4c1151c6c3fbd2577a20e1d97fb
kalunge <muthomititus+github@gmail.com>
2021-10-14 10:20:39 +0100

    Fix failing tests and remove commented code

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,7 +2,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { FaBible } from 'react-icons/fa';
 import { authenticate, getBooks } from '../redux/books/books';
-// import bible from '../img/bible.jpg';
 import Book from './Book';
 import Header from './Header';
 
